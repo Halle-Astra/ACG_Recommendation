e2f65496321a867212ff999ced4e8d333ecea068
Halle-Astra <halle.wang@qq.com>
2020-10-23 00:09:22 +0800

    Dataset Found

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 ## bangumi\_spider
 爬虫部分，编写爬虫获取数据。
 
+鉴于爬虫编写太费时，此计划暂时搁置，改为使用开源数据集。
+
+## Dataset Used
+使用数据集为Anime Recommendations Database.该数据集包含来自12,294个动漫的73,516个用户的用户偏好数据。每个用户都可以将动画添加到已完成的列表中并为其评分，该数据集把这些评级整理起来。
+
+· 下载链接：https://www.kaggle.com/CooperUnion/anime-recommendations-database
+
+摘自https://blog.csdn.net/hellozhxy/article/details/81275133
+
 ## Diary
 ### 2020.10.22
 1. 完成了第一个按时间排序的页面的爬取尝试。
--- spider_bangumi.py
+++ spider_bangumi.py
@@ -20,4 +20,7 @@ def cget(root):
 root = r'https://bgm.tv/anime/browser/?sort=date'
 #print(rqget(root))# So I will discard this method to get page source and utilize selenium to do it 
 #I need complete this work(scripts) soon so I don't care the speed of spider .
-print(cget(root))
+#print(cget(root))
+c.get(root)
+page_num = c.find_element_by_class_name('p_edge').text.split('/')[1].split(')')[0].strip()
+print(page_num)
