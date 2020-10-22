a40c0d118244f4400e27fcb1753c87cb06b5650d
Halle-Astra <halle.wang@qq.com>
2020-10-22 20:30:57 +0800

    Spider scripts commit on 2020.10.22 first

---
+++ spider_bangumi.py
@@ -0,0 +1,23 @@
+import requests as rq 
+from lxml import etree 
+import os 
+from selenium.webdriver import Chrome
+
+c = Chrome()
+
+def rqget(root):
+    r = rq.get(root)
+    r.encoding = r.apparent_encoding 
+    #if r.response_code == '200':
+    return r.text
+
+def cget(root):
+    c.get(root)
+    txt = c.page_source
+    c.close()
+    return txt
+
+root = r'https://bgm.tv/anime/browser/?sort=date'
+#print(rqget(root))# So I will discard this method to get page source and utilize selenium to do it 
+#I need complete this work(scripts) soon so I don't care the speed of spider .
+print(cget(root))
